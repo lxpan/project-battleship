0b80aafd7dd2eaa4d1cf71398050825f758d69f4
Luoxi Pan <l.panpax@gmail.com>
2023-01-19 17:18:33 +1100

    Remove view tests for now

--- tests/view.test.js
+++ tests/view.test.js
@@ -3,17 +3,4 @@
  */
 import View from '../src/View';
 
-test('drawBoard able to create 10x10 table', () => {
-    document.body.innerHTML = `
-      <div id="boardDiv"></div>
-    `;
-
-    const tableDiv = document.getElementById('boardDiv');
-    const table = View.drawBoard(10, 10);
-
-    tableDiv.appendChild(table);
-    const rows = tableDiv.querySelectorAll('tr');
-
-    // const rows = document.querySelectorAll('tr');
-    console.log(tableDiv.innerHTML);
-});
+test('drawBoard able to create 10x10 table', () => {});
